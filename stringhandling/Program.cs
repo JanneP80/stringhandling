93edd13bfb277f66007d6ba794944edc7a97ab1c
JanneP80 <janne.puolitaival@student.saimia.fi>
2017-02-21 12:19:09 +0200

    added two short string editing and handling tasks

---
+++ stringhandling/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringhandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // char counter from user input
+            Console.WriteLine("Input some text for calculating amount of chars: ");
+            string stringforcount = Console.ReadLine();
+            Console.WriteLine("Length was: {0}", stringforcount.Length);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ strings2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strings2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // char counter from user input
+            Console.WriteLine("Input some text for changing letters 'e' to '@': ");
+            string stringtochange = Console.ReadLine();
+            Console.WriteLine("Modification is: '{0}'", stringtochange.Replace("e", "@"));
+            Console.ReadKey();
+        }
+    }
+}
