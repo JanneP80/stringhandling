5a2b50f426e2bd6ae72d725c122611ddc7cb222d
JanneP80 <janne.puolitaival@student.saimia.fi>
2017-02-21 13:38:52 +0200

    Added incomplete capsules

---
+++ stringhandling4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringhandling4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountl = 0;
+            // char counter from user input
+            Console.WriteLine("Input some text for calculationg letters 'L': ");
+            string stringtocalc = Console.ReadLine().ToUpper();
+            for (int i = 0; i < stringtocalc.Length; i++)
+            {
+                if (stringtocalc[i] == ('L'))
+                {
+                    amountl++;
+                }
+            }
+
+            Console.WriteLine("Amount of 'L' is: '{0}'", amountl);
+            Console.ReadKey();
+        }
+    }
+}
